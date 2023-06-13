a37600fd6e8ad3daabc0bd07fc2a8e40682575a3
xTech/beintouch01 <xtech.beintouch01@gmail.com>
2023-06-13 23:19:44 +0300

    swift hello world

---
+++ metal_triangle/metal_triangle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  metal_triangle
+//
+//  Created by Stella on 13.6.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ metal_triangle/metal_triangle/metal_triangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  metal_triangleApp.swift
+//  metal_triangle
+//
+//  Created by Stella on 13.6.2023.
+//
+
+import SwiftUI
+
+@main
+struct metal_triangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
