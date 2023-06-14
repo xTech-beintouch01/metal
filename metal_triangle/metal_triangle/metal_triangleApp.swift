9dbf7b7c8e9a189839bc922d41eb8f12ed5fcc45
stella-te <stella@tradingeconomics.com>
2023-06-15 00:22:40 +0300

    metal_triangle control ui view

--- metal_triangle/metal_triangle/metal_triangleApp.swift
+++ metal_triangle/metal_triangle/metal_triangleApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct metal_triangleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // ContentView()
+            metal_triangle()
         }
     }
 }
